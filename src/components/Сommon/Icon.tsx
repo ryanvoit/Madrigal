70558dd16f62ffabece7e8c0778c4376c2d6e5a1
Олег <xoxoxo.xoxo@mail.ru>
2026-07-09 18:41:13 +0500

    SVG | Animation - update

--- src/components/Сommon/Icon.tsx
+++ src/components/Сommon/Icon.tsx
@@ -7,14 +7,13 @@ interface IconProps {
 export const Icon: FC<IconProps & React.SVGProps<SVGSVGElement>> = ({
     role,
     className,
-    ...props
 }) => {
     switch (role) {
         case 'microphone':
             return (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
                     className={className}
-                    {...props}>
+                    >
                     <rect x="9" y="2" width="6" height="12" rx="3" fill="currentColor" />
                     <path d="M5 11a7 7 0 0 0 14 0" stroke="currentColor" strokeWidth="2" strokeLinecap="round" fill="none"
                     />
@@ -29,7 +28,7 @@ export const Icon: FC<IconProps & React.SVGProps<SVGSVGElement>> = ({
             return (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                     className={className}
-                    {...props}>
+                    >
                     <circle cx="12" cy="12" r="8" fill="#e11d48" />
                 </svg>
             );
@@ -38,7 +37,7 @@ export const Icon: FC<IconProps & React.SVGProps<SVGSVGElement>> = ({
             return (
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
                     className={className}
-                    {...props}>
+                    >
                     <circle cx="11" cy="11" r="8" stroke="currentColor" strokeWidth="2" fill="none" />
                     <line x1="21" y1="21" x2="16.65" y2="16.65" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                 </svg>
